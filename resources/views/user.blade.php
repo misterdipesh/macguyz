3446a43c7bb7ddf24a073ea8aca6b32c7374bda5
lokrajthapa <lokrajthapa741@gmail.com>
2020-01-23 12:54:16 -0800

    first commit

---
+++ resources/views/user.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>this is user </title>
+</head>
+<body>
+
+<form action="/result" method="get">
+  file id:<br>
+  <input type="text" name="fid"><br>
+  file pin:<br>
+  <input type="text" name="fpin">
+  <br>
+
+  <button type="submit">login</button>
+  
+</form> 
+
+  
+
+</body>
+</html>
